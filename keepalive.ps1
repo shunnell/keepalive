7c4c7506e65574d4976d9a41c4d564e3da461f04
Steve Hunnell <steve.hunnell@outlook.com>
2023-08-14 10:50:49 -0400

    Update keepalive.ps1

--- keepalive.ps1
+++ keepalive.ps1
@@ -3,12 +3,12 @@ $quit_time = '5:00:00pm'  # Modify quit_time for when you want the program to st
 
 while ($true) {
    $wsh.SendKeys('+{SCROLLLOCK}')
-   $wsh.SendKeys('+{SCROLLLOCK}')
+   $wsh.SendKeys('+{SCROLLLOCK}') # Keep the scroll-lock either off or on by entering it twice
    $RNDSleep = Get-Random -minimum 1 -maximum 15
    Write-Output $RNDSleep
    Start-Sleep -seconds $RNDSleep
    if ((Get-Date) -gt (Get-Date -Date $quit_time)) {
-      shutdown /l
+      shutdown /l  # caution: this will log off your current session!
       break
    }
 }
