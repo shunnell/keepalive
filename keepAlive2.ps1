871359c68725017902d76c7ac34d36751603d9ea
Steve Hunnell <steve.hunnell@outlook.com>
2024-10-11 07:45:01 -0400

    Create keepAlive2.ps1

---
+++ keepAlive2.ps1
@@ -0,0 +1,33 @@
+# CAUTION: This script will prevent your screen-saver, or power management from activating.
+# Useful if you are transcribing from one computer to another and you get tired of moving the mouse to re-activate
+#    (Sometimes it is near impossible to transfer from one computer to another...for example, when working in a
+#    classified environment, and two or more networks are isolated from one another, and USBs are strictly prohibited)
+
+$wsh = New-Object -ComObject WScript.shell
+$quit_time = '5:00:00pm'  # Modify quit_time for when you want the program to stop
+
+$RNDSleep = 5 
+while ($true) {
+  $wsh.SendKeys('+{SCROLLLOCK}')
+  $wsh.SendKeys('+{SCROLLLOCK}') # Keep the scroll-lock either off or on by entering it twice
+  if ($RNDSleep % 5 -eq 0) {
+     Write-Output "FAST LOOP INITIATED!"
+     $counter = 1
+     $COUNTERRandom = Get-Random -minimum 8 -maximum 20
+     while ($counter -lt $COUNTERRandom) {
+        $RNDSleep = Get-Random -minimum 927 -maximum 4091
+        # Send the key twice to keep it either on or off
+        $wsh.SendKeys('+{SCROLLLOCK}')
+        $wsh.SendKeys('+{SCROLLLOCK}')
+        Start-Sleep -Milliseconds $RNDSleep
+        $counter += 1
+        Write-Output $RNDSleep
+     }
+  }
+  $RNDSleep = Get-Random -minimum 727 -maximum 16001
+  Write-Output $RNDSleep
+  Start-Sleep -milliseconds $RNDSleep
+  if ((Get-Date) -gt (Get-Date -Date $quit_time)) {
+     break
+  }
+}
